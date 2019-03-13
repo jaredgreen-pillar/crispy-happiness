c11b8910ef46f20a1ed9f828dbfa2b8896ba1b10
NatalieFrecka <nfrecka@gmail.com>
2019-03-13 17:01:16 -0500

    Camera up and running. Haven't tried connecting yet.

--- app/src/main/java/com/example/crispyhappiness/MainActivity.kt
+++ app/src/main/java/com/example/crispyhappiness/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.crispyhappiness
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.vidyo.VidyoClient.Connector.Connector
 import com.vidyo.VidyoClient.Connector.ConnectorPkg
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Connector.IConnect {
     private lateinit var vc: Connector
@@ -15,18 +16,52 @@ class MainActivity : AppCompatActivity(), Connector.IConnect {
 
         ConnectorPkg.setApplicationUIContext(this)
         ConnectorPkg.initialize()
+
+        button.setOnClickListener {
+            start()
+        }
+
+        button2.setOnClickListener {
+            connect()
+        }
+
+        button3.setOnClickListener {
+            disconnect()
+        }
     }
 
-    override fun onSuccess() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun start() {
+        vc = Connector(
+            videoFrame,
+            Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default,
+            15,
+            "warning info@VidyoClient info@VidyoConnector",
+            "",
+            0
+        )
+        vc.showViewAt(videoFrame, 0, 0, videoFrame.width, videoFrame.height)
     }
 
-    override fun onFailure(p0: Connector.ConnectorFailReason?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun connect() {
+        val token = "INSERT TOKEN HERE"
+        vc.connect("prod.vidyo.io", token, "DemoUser", "DemoRoom", this)
     }
 
-    override fun onDisconnected(p0: Connector.ConnectorDisconnectReason?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun disconnect() {
+        vc.disconnect()
     }
 
+    override fun onSuccess() {}
+
+    override fun onFailure(reason: Connector.ConnectorFailReason?) {
+        logFail(reason.toString())
+    }
+
+    override fun onDisconnected(reason: Connector.ConnectorDisconnectReason?) {
+        logFail(reason.toString())
+    }
+
+    fun logFail(reason: String?) {
+        Log.d("VIDYOFAIL", reason ?: "UNDEFINED")
+    }
 }
